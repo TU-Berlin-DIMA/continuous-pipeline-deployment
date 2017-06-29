c570ae758f8cea40013ad7eae9d452c8829d808f
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2017-06-29 10:12:47 +0200

    [SIGMOD-2018] finializes the criteo dataset preprocessing

--- code/spark/continuous-training/src/main/scala/de/dfki/examples/CriteoPreprocessing.scala
+++ code/spark/continuous-training/src/main/scala/de/dfki/examples/CriteoPreprocessing.scala
@@ -1,11 +1,15 @@
 package de.dfki.examples
 
+import java.io.IOException
+
 import de.dfki.utils.CommandLineParser
-import org.apache.spark.SparkConf
-import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer, VectorAssembler}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.types.{DoubleType, StringType}
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.{SparkConf, sql}
+
 
 /**
   * Preprocess the Crieto dataset by using one hot encoder on all the categorical features
@@ -16,29 +20,97 @@ import org.apache.spark.sql.types.{DoubleType, StringType}
   *
   */
 object CriteoPreprocessing {
-  val INPUT_PATH = "data/criteo-full/"
-  val OUTPUT_PATH = "data/criteo-full/processed/"
+  val INPUT_PATH = "data/criteo-full"
+  val OUTPUT_PATH = "data/criteo-full/processed"
+  // There are a total of 23 days
+  val DAYS = 4
 
   def main(args: Array[String]): Unit = {
 
     val parser = new CommandLineParser(args).parse()
-    val data = parser.get("input-path", INPUT_PATH)
+    val root = parser.get("input-path", INPUT_PATH)
     val result = parser.get("output-path", OUTPUT_PATH)
+    val days = 0 to parser.getInteger("days", DAYS)
 
     val conf = new SparkConf().setAppName("Criteo Feature Engineering")
     val masterURL = conf.get("spark.master", "local[*]")
+    conf.set("spark.executor.memory", "5g")
     conf.setMaster(masterURL)
 
     val spark = SparkSession.builder()
       .config(conf)
       .getOrCreate()
 
+    var indexerModel: PipelineModel = _
+    try {
+      // load the model if it already exists
+      indexerModel = PipelineModel.load(s"$root/model/")
+    }
+    catch {
+      case iie: IOException =>
+        indexerModel = createIndexerModel(spark, root)
+    }
+
+    // use the indexer model to transform the data
+    // this way we are ensuring that the one hot encoding is unified accross all days
+    for (d <- days) {
+      val dataFrame = spark.read
+        .format("csv")
+        .option("delimiter", ",")
+        .option("header", "false")
+        .option("inferSchema", "true")
+        .load(s"$root/raw/$d")
+
+      // add missing values NULL for string and 0.0 for double
+      var columnMapping = collection.mutable.Map[String, Any]()
+      for (t <- dataFrame.schema.fields) {
+        if (t.dataType == StringType) {
+          columnMapping += (t.name -> "NULL")
+        } else if (t.dataType == DoubleType) {
+          columnMapping += (t.name -> 0.0)
+        }
+      }
+      val map = Map() ++ columnMapping
+      val filledDf = dataFrame.na.fill(map)
+
+      val indexedDf = indexerModel.transform(filledDf)
+
+      val indexColumns = indexedDf.columns.filter(x => x contains "index")
+
+      val oneHotEncoder: Array[org.apache.spark.ml.PipelineStage] = indexColumns.map(
+        cname => new OneHotEncoder()
+          .setInputCol(cname)
+          .setOutputCol(s"${cname}_vec")
+      )
+      val oneHotPipeline = new Pipeline().setStages(oneHotEncoder)
+      val finalDf = oneHotPipeline.fit(indexedDf).transform(indexedDf)
+
+      // assemble the preprocessed columns into vectors
+      val finalColumns = (1 to 13).map(v => s"_c$v") ++ finalDf.columns.filter(c => c contains "vec")
+      val assembler = new VectorAssembler()
+        .setInputCols(finalColumns.toArray)
+        .setOutputCol("features")
+
+      val output = assembler.transform(finalDf).select("_c0", "features")
+      // without explicitly caching the dataset, the program runs out of memory on my local machine
+      output.cache()
+      output.count()
+
+      finalDf.unpersist(true)
+      dataFrame.unpersist(true)
+      output.rdd.map(r => (r.get(0).toString, r.get(1).toString)).saveAsTextFile(s"$result/$d/")
+      output.unpersist(true)
+    }
+
+  }
+
+  def createIndexerModel(spark: SparkSession, root: String): PipelineModel = {
     val df = spark.read
       .format("csv")
       .option("delimiter", ",")
       .option("header", "false")
       .option("inferSchema", "true")
-      .load(data)
+      .load(s"$root/all")
 
 
     // add missing values NULL for string and 0.0 for double
@@ -63,40 +135,57 @@ object CriteoPreprocessing {
           .setOutputCol(s"${colName}_index")
     }
 
+    // train an indexer on the entire data
+    // then use it to index and encode individual datasets
     val indexerPipeline = new Pipeline().setStages(indexTransformers.toArray)
-    val indexedDf = indexerPipeline.fit(filledDf).transform(filledDf)
+    val indexerModel = indexerPipeline.fit(filledDf)
+    filledDf.unpersist(true)
+    df.unpersist(true)
+    indexerModel
+  }
+
+}
 
 
-    // transform categorical columns using the one hot encoder
-    val indexColumns = indexedDf.columns.filter(x => x contains "index")
-    val oneHotEncoder: Array[org.apache.spark.ml.PipelineStage] = indexColumns.map(
-      cname => new OneHotEncoder()
-        .setInputCol(cname)
-        .setOutputCol(s"${cname}_vec")
-    )
-    val oneHotPipeline = new Pipeline().setStages(oneHotEncoder)
-    val finalDf = oneHotPipeline.fit(indexedDf).transform(indexedDf)
+// On the local version we are working with a sample and as a result not all of the
+// days will contains the same set of categorical variables
+// In order to do one hot encoding of the data, we have to combine all the data, make the necessary
+// transformations and then separate them
+object AddDayColumn {
+  val INPUT_PATH = "data/criteo-full/raw"
+  val OUTPUT_PATH = "data/criteo-full/all"
 
+  def main(args: Array[String]): Unit = {
+    val parser = new CommandLineParser(args).parse()
+    val root = parser.get("input-path", INPUT_PATH)
+    val result = parser.get("output-path", OUTPUT_PATH)
 
-    // assemble the preprocessed columns into vectors
-    val finalColumns = (1 to 13).map(v => s"_c$v") ++ finalDf.columns.filter(c => c contains "vec")
-    val assembler = new VectorAssembler()
-      .setInputCols(finalColumns.toArray)
-      .setOutputCol("features")
+    val days = 0 to 4
 
-    val output = assembler.transform(finalDf)
-    output.createOrReplaceTempView("result")
+    val conf = new SparkConf().setAppName("Criteo Feature Engineering")
+    val masterURL = conf.get("spark.master", "local[*]")
+    conf.set("spark.executor.memory", "5g")
+    conf.setMaster(masterURL)
 
-    // store the preprocessed data
-    // _c40 represent the day number, it was added to the source dataset inorder to have consistent one hot encoding
-    // scheme
-    val output0 = spark.sql("select _c0, features from result where _c40 = 0")
-    output0.rdd.map(r => (r.get(0), r.get(1))).saveAsTextFile(s"$result/0/")
+    val spark = SparkSession.builder()
+      .config(conf)
+      .getOrCreate()
 
-    val output1 = spark.sql("select _c0, features from result where _c40 = 1")
-    output1.rdd.map(r => (r.get(0), r.get(1))).saveAsTextFile(s"$result/1/")
+    for (d <- days) {
+      val day = processDay(spark, root, d)
+      day.write.option("header", "false").mode(SaveMode.Append).csv(path = result)
+    }
 
   }
 
+  def processDay(spark: SparkSession, root: String, day: Int): sql.DataFrame = {
+    val df = spark.read
+      .format("csv")
+      .option("delimiter", ",")
+      .option("header", "false")
+      .option("inferSchema", "true")
+      .load(s"$root/$day/")
+    df.withColumn("day", lit(day))
+  }
 }
 
