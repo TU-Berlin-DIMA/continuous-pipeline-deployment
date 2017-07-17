a2c5ba138471ef62ff601c9bb51c00a2d645e492
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2017-07-17 11:17:10 +0200

    [ICDE-2018] make confusion matrix serializable and add read and write csv methods

--- code/spark/continuous-training/src/main/scala/de/dfki/ml/evaluation/ConfusionMatrix.scala
+++ code/spark/continuous-training/src/main/scala/de/dfki/ml/evaluation/ConfusionMatrix.scala
@@ -6,16 +6,38 @@ package de.dfki.ml.evaluation
 class ConfusionMatrix(val tp: Int,
                       val fp: Int,
                       val tn: Int,
-                      val fn: Int) {
+                      val fn: Int) extends Serializable {
 
-  val accuracy = (tp + tn).toDouble / (tp + fp + tn + fn)
-  val precision = tp.toDouble / (tp + fp)
-  val recall = tp.toDouble / (tp + fn)
-  val fMeasure = (2 * precision * recall) / (precision + recall)
-  val size = tp + fp + tn + fn
+  calculate()
+
+  def calculate() {
+    accuracy = (tp + tn).toDouble / (tp + fp + tn + fn)
+    precision = tp.toDouble / (tp + fp)
+    recall = tp.toDouble / (tp + fn)
+    fMeasure = (2 * precision * recall) / (precision + recall)
+    size = tp + fp + tn + fn
+
+  }
+
+  var accuracy: Double = _
+  var precision: Double = _
+  var recall: Double = _
+  var fMeasure: Double = _
+  var size: Int = _
 
   override def toString = {
     s"accuracy($accuracy), precision($precision), recall($recall), f-measure($fMeasure)"
   }
 
+  // tp, fp, tn, fn
+  def asCSV: String = {
+    s"$tp,$fp,$tn,$fn"
+  }
+}
+
+object ConfusionMatrix {
+  def parseCSV(csv: String): ConfusionMatrix = {
+    val parsed = csv.split(",").map(_.trim).map(_.toInt)
+    new ConfusionMatrix(parsed(0), parsed(1), parsed(2), parsed(3))
+  }
 }
