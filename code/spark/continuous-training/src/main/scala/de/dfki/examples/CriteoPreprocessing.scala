84e83ef0c644fc85872f9db71582cb26e858ae5e
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2017-10-13 17:44:15 +0200

    [SIGMOD-2018] improves criteo pre processing for cluster execution

--- code/spark/continuous-training/src/main/scala/de/dfki/examples/CriteoPreprocessing.scala
+++ code/spark/continuous-training/src/main/scala/de/dfki/examples/CriteoPreprocessing.scala
@@ -7,7 +7,7 @@ import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer, VectorAssemble
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.types.{DoubleType, StringType}
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.{SparkConf, sql}
 
 
@@ -190,6 +190,125 @@ object AddDayColumn {
       .option("inferSchema", "true")
       .load(s"$root/$day/")
     df.withColumn("day", lit(day))
+  }
+}
+
+object CriteoPreprocessingOneDay {
+  val INPUT_PATH = "data/criteo-full"
+  val OUTPUT_PATH = "data/criteo-full/processed"
+  val INDEXER_MODEL_PATH = "data/criteo-full/model"
+  val FILES_PER_DAY = 100
+
+  def main(args: Array[String]): Unit = {
+
+    val parser = new CommandLineParser(args).parse()
+    val inputData = parser.get("input-path", INPUT_PATH)
+    val resultPath = parser.get("output-path", OUTPUT_PATH)
+    val filesPerDay = parser.getInteger("files-per-day", FILES_PER_DAY)
+    val modelPath = parser.get("model-path", INDEXER_MODEL_PATH)
+
+    val conf = new SparkConf().setAppName("Criteo Feature Engineering")
+    val masterURL = conf.get("spark.master", "local[*]")
+    conf.setMaster(masterURL)
+
+    val spark = SparkSession.builder()
+      .config(conf)
+      .getOrCreate()
+
+    val dataFrame = spark.read
+      .format("csv")
+      .option("delimiter", "\t")
+      .option("header", "false")
+      .option("inferSchema", "true")
+      .load(s"$inputData")
+
+    dataFrame.cache()
+    dataFrame.count()
+    var indexerModel: PipelineModel = null
+    try {
+      // load the model if it already exists
+      indexerModel = PipelineModel.load(s"$inputData/model/")
+    }
+    catch {
+      case iie: IOException =>
+        indexerModel = createIndexerModel(spark, dataFrame, modelPath)
+    }
+    // add missing values NULL for string and 0.0 for double
+    var columnMapping = collection.mutable.Map[String, Any]()
+    for (t <- dataFrame.schema.fields) {
+      if (t.dataType == StringType) {
+        columnMapping += (t.name -> "NULL")
+      } else if (t.dataType == DoubleType) {
+        columnMapping += (t.name -> 0.0)
+      }
+    }
+    val map = Map() ++ columnMapping
+    val filledDf = dataFrame.na.fill(map)
+
+    val indexedDf = indexerModel.transform(filledDf)
+
+    val indexColumns = indexedDf.columns.filter(x => x contains "index")
+
+    val oneHotEncoder: Array[org.apache.spark.ml.PipelineStage] = indexColumns.map(
+      cname => new OneHotEncoder()
+        .setInputCol(cname)
+        .setOutputCol(s"${cname}_vec")
+    )
+    val oneHotPipeline = new Pipeline().setStages(oneHotEncoder)
+    val finalDf = oneHotPipeline.fit(indexedDf).transform(indexedDf)
+
+    dataFrame.unpersist(true)
+    // assemble the preprocessed columns into vectors
+    val finalColumns = (1 to 13).map(v => s"_c$v") ++ finalDf.columns.filter(c => c contains "vec")
+    val assembler = new VectorAssembler()
+      .setInputCols(finalColumns.toArray)
+      .setOutputCol("features")
+
+    val output = assembler.transform(finalDf).select("_c0", "features").repartition(filesPerDay)
+    // without explicitly caching the dataset, the program runs out of memory on my local machine
+    output.cache()
+    output.count()
+
+
+    finalDf.unpersist(true)
+    dataFrame.unpersist(true)
+    output.rdd.map(r => s"${r.get(0).toString} | ${r.get(1).toString}").saveAsTextFile(s"$resultPath/")
+    output.unpersist(true)
+
+
+  }
+
+  def createIndexerModel(spark: SparkSession, df: DataFrame, modelOutput: String): PipelineModel = {
+    // add missing values NULL for string and 0.0 for double
+    var columnMapping = collection.mutable.Map[String, Any]()
+    for (t <- df.schema.fields) {
+      if (t.dataType == StringType) {
+        columnMapping += (t.name -> "NULL")
+      } else if (t.dataType == DoubleType) {
+        columnMapping += (t.name -> 0.0)
+      }
+    }
+    val map = Map() ++ columnMapping
+    val filledDf = df.na.fill(map)
+
+
+    // add index for all the categorical columns
+    val stringColumns = (14 to 39).map(d => s"_c$d")
+    val indexTransformers = stringColumns.map {
+      colName =>
+        new StringIndexer()
+          .setInputCol(colName)
+          .setOutputCol(s"${colName}_index")
+    }
+
+    // train an indexer on the entire data
+    // then use it to index and encode individual datasets
+    val indexerPipeline = new Pipeline().setStages(indexTransformers.toArray)
+    val indexerModel = indexerPipeline.fit(filledDf)
+    filledDf.unpersist(true)
+    df.unpersist(true)
+    indexerModel.save(s"$modelOutput")
+    indexerModel
   }
 }
 
