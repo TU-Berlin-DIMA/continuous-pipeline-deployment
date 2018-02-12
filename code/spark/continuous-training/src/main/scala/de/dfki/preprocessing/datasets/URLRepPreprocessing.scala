d0ba6d81526398c36da6696ca5fd99661de018f8
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2018-02-12 14:45:36 +0100

    simplifies the url rep dataset, similar to criteo data

--- code/spark/continuous-training/src/main/scala/de/dfki/preprocessing/datasets/URLRepPreprocessing.scala
+++ code/spark/continuous-training/src/main/scala/de/dfki/preprocessing/datasets/URLRepPreprocessing.scala
@@ -1,12 +1,6 @@
 package de.dfki.preprocessing.datasets
 
-import de.dfki.preprocessing.Preprocessor
 import de.dfki.utils.CommandLineParser
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.io.NullWritable
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -14,15 +8,14 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object URLRepPreprocessing {
   val INPUT_PATH = "data/url-reputation/raw"
-  val OUTPUT_PATH = "data/url-reputation-sample"
-  val FILE_COUNT = 1
-  val SAMPLING_RATE = 0.05
+  val OUTPUT_PATH = "data/url-reputation/processed"
+  val FILE_COUNT = 100
+  val SAMPLING_RATE = 1.0
 
   def main(args: Array[String]): Unit = {
     val parser = new CommandLineParser(args).parse()
     val conf = new SparkConf().setAppName("URL Data")
     val masterURL = conf.get("spark.master", "local[*]")
-    conf.set("spark.hadoop.validateOutputSpecs", "false")
     conf.setMaster(masterURL)
     val sc = new SparkContext(conf)
     val inputPath = parser.get("input-path", INPUT_PATH)
@@ -32,28 +25,16 @@ object URLRepPreprocessing {
 
     val data = {
       if (samplingRate < 1.0)
-        MLUtils.loadLibSVMFile(sc, s"$inputPath/Day0.svm").sample(withReplacement = false, fraction = samplingRate, seed = 42)
+        sc.textFile(s"$inputPath/Day0.svm").sample(withReplacement = false, fraction = samplingRate, seed = 42)
       else
-        MLUtils.loadLibSVMFile(sc, s"$inputPath/Day0.svm")
-    }.map(l => new LabeledPoint(if (l.label == -1.0) 0 else 1.0, l.features))
+        sc.textFile(s"$inputPath/Day0.svm")
+    }
 
-    Preprocessor.convertToSVM(data).repartition(sc.defaultParallelism).saveAsTextFile(s"$outputPath/initial-training/")
+    data.saveAsTextFile(s"$outputPath/initial-training/day_0")
 
-
-    for (i <- 1 until 120) {
-      val data = MLUtils.loadLibSVMFile(sc, s"$inputPath/Day$i.svm")
-        .map(l => new LabeledPoint(if (l.label == -1.0) 0 else 1.0, l.features))
-      val hadoopConf = new Configuration()
-      hadoopConf.set("mapreduce.output.basename", s"day${"%05d".format(i)}")
-      val processed =
-        if (samplingRate < 1.0)
-          Preprocessor.convertToSVM(data).repartition(fileCount)
-            .sample(withReplacement = false, fraction = samplingRate, seed = 42)
-        else
-          Preprocessor.convertToSVM(data).repartition(fileCount)
-
-      processed.map(str => (null, str)).saveAsNewAPIHadoopFile(s"$outputPath/stream-training/", classOf[NullWritable], classOf[String],
-        classOf[TextOutputFormat[NullWritable, String]], hadoopConf)
+    for (i <- 1 to 120) {
+      val data = sc.textFile(s"$inputPath/Day$i.svm")
+      data.repartition(fileCount).saveAsTextFile(s"$outputPath/stream/day_$i")
     }
   }
 
