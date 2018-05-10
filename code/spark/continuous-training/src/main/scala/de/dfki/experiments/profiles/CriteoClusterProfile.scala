55c0cb490544112fc72192c7b3210df7cdd300ce
Behrouz Derakhshan <behrouz.derakhshan@dfki.de>
2018-05-10 15:36:00 +0200

    [CIKM-2018] adds criteo hyperparameter tuning and fixes the criteo experiment profile

--- code/spark/continuous-training/src/main/scala/de/dfki/experiments/profiles/CriteoClusterProfile.scala
+++ code/spark/continuous-training/src/main/scala/de/dfki/experiments/profiles/CriteoClusterProfile.scala
@@ -7,12 +7,13 @@ class CriteoClusterProfile extends Profile{
   override val INPUT_PATH = "hdfs://cloud-11:44000/user/behrouz/criteo/experiments/initial-training/day=0"
   override val STREAM_PATH = "hdfs://cloud-11:44000/user/behrouz/criteo/experiments/stream"
   override val MATERIALIZED_PATH = "hdfs://cloud-11:44000/user/behrouz/criteo/experiments/materialized"
+  override val BATCH_EVALUATION: String = "hdfs://cloud-11:44000/criteo/day_1"
   override val EVALUATION_PATH = "prequential"
-  override val RESULT_PATH = "/share/hadoop/behrouz/experiments/criteo-full/sampling"
-  override val INITIAL_PIPELINE = "/share/hadoop/behrouz/experiments/criteo-full/sampling-mode-prequential/pipelines/init_500"
+  override val RESULT_PATH = "/share/hadoop/behrouz/experiments/criteo/results/"
+  override val INITIAL_PIPELINE = "/share/hadoop/behrouz/experiments/criteo/pipelines/best/rmsprop-1.0E-4"
   override val DELIMITER = "\t"
   override val NUM_FEATURES = 3000
-  override val NUM_ITERATIONS = 500
+  override val NUM_ITERATIONS = 5000
   override val SLACK = 10
   override val DAYS = "1,5"
   override val SAMPLE_SIZE = 1440
